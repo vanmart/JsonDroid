bcba917bcf757abd5e716d473b4bc7abcf8e309f
Angel Celis Botto <angelbotto@gmail.com>
2012-07-19 07:34:44 -0500

    Jugando con json

---
+++ gen/labs/kreattiewe/jsondroid/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package labs.kreattiewe.jsondroid;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/labs/kreattiewe/jsondroid/MainActivity.java
@@ -0,0 +1,84 @@
+package labs.kreattiewe.jsondroid;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    	
+        
+        String readTwitterFeed = readTwitterFeed();
+        try {
+			JSONArray jsonArray = new JSONArray(readTwitterFeed);
+			Log.i(MainActivity.class.getName(),
+					"Numero de entradas " + jsonArray.length());
+			
+			for (int i = 0; i < jsonArray.length(); i++) {
+				JSONObject jsonObject = jsonArray.getJSONObject(i);
+				Log.i(MainActivity.class.getName(), jsonObject.getString("nombre"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    public String readTwitterFeed() {
+   	
+        StringBuilder builder = new StringBuilder();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet("http://androides.herokuapp.com/friends.json");
+		try {
+			HttpResponse response = client.execute(httpGet);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+			if (statusCode == 200) {
+				HttpEntity entity = response.getEntity();
+				InputStream content = entity.getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+				}
+			} else {
+				Log.e(MainActivity.class.toString(), "Error al descargar json");
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return builder.toString();
+	}
+
+    
+}
